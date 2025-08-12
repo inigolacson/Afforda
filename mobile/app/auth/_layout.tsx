6245c6e52b9ca3c6bc3a374ebfd9aa60499f308e
inigolacson <inigolacson01@gmail.com>
2025-08-12 16:41:31 +0800

    fix: bundling auth-layout-index error

---
+++ mobile/app/auth/_layout.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+import { Stack } from "expo-router";
+export { StatusBar } from "expo-status-bar";
+
+export default function AuthLayout() {
+    return  <Stack/>
+}
---
+++ mobile/app/auth/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function LoginPage() {
+    return (
+        <View>
+            <Text>
+                Hello
+            </Text>
+        </View>
+    )
+}
