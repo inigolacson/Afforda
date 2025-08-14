c2ad154c50169de8744e48a915899291f0cf69cf
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-08-14 17:13:11 +0800

    feat: added sliding animation

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,11 +5,12 @@ import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <View className="flex-1 bg-[#161616]">
-      <StatusBar style="light" />
+    <View className="flex-1 bg-[#FFFFFF]">
+      <StatusBar style="auto" />
       <Stack
         screenOptions={{
           headerShown: false,
+          animation: "slide_from_right",
           contentStyle: {
             backgroundColor: "#FFFFFF",
           },
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,25 +3,29 @@ import { Link } from "expo-router";
 
 export default function WelcomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 w-full h-full py-8 px-16 justify-center items-center">
       {/* Image or Logo */}
       <View>
         <Text></Text>
       </View>
 
       {/* Sign In Button */}
-      <View className="items-center w-3/4 mb-4">
+      <View className="w-full mb-4 items-center">
         <Link href="/auth" asChild>
-          <TouchableOpacity>
-            <Text>Hi</Text>
+          <TouchableOpacity className="w-3/4 shawdow-md bg-black py-4 px-8 rounded-full items-center">
+            <Text className="text-white text-2xl tracking-widest font-bold">
+              Hi
+            </Text>
           </TouchableOpacity>
         </Link>
       </View>
       {/* Sign Up Button */}
-      <View>
+      <View className="w-full mb-4 items-center">
         <Link href="/auth/sign-up" asChild>
-          <TouchableOpacity>
-            <Text>Hello</Text>
+          <TouchableOpacity className="w-3/4 shadow-md bg-black py-4 px-8 rounded-full items-center">
+            <Text className="text-white text-2xl tracking-widest font-bold">
+              Hello
+            </Text>
           </TouchableOpacity>
         </Link>
       </View>
