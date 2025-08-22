314d1b69647db30cfd787da0826dd5f69b03b638
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-08-22 20:25:45 +0800

    feat: start on routes

--- backend/src/app/api/deductions/[deductionId]/route.ts
+++ backend/src/app/api/deductions/[deductionId]/route.ts
@@ -1,2 +1,9 @@
 import { NextRequest } from "next/server";
 import { deductionsController } from "@controllers/deductionsController";
+
+//create deductions
+export async function POST(req: NextRequest) {
+  return;
+}
+
+//get deduction
