cc32f1008a7fac68e063de36062806ffc75a8d17
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-09-05 15:32:48 +0800

    feat: correct type in google client

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -18,7 +18,7 @@ export const auth = betterAuth({
     },
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLEC_CLIENT_SECRET as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       redirectURI: "https://afforda-zeta.vercel.app/api/auth/callback/google",
     },
   },
