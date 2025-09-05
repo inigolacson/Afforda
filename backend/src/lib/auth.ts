e43fbd38013d8ab91531712b3c02b759a12459e3
inigolacson <132420301+inigolacson@users.noreply.github.com>
2025-09-05 16:50:54 +0800

    feat: added RedirectURI

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -14,10 +14,12 @@ export const auth = betterAuth({
     facebook: {
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+      redirectURI: "https://afforda-zeta.vercel.app/api/auth/callback/facebook",
     },
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectURI: "https://afforda-zeta.vercel.app/api/auth/callback/google",
     },
   },
   emailAndPassword: {
